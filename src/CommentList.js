c19cdfff683ca43ba719ad8ea9aa04729eae139c
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2023-04-11 16:44:07 +0200

    form box
    added the comment box but for some reason two rendered

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { FormControl } from 'react-bootstrap';
 
 import CommentListItem from './CommentListItem';
 
 class CommentList extends Component {
         
     state = {
-
+        commentFilter:""
         }
     
     render() {
         return (
+          <Fragment>
+            <FormControl value={this.state.commentFilter} onChange={(e) => this.setState({ commentFilter: e.currentTarget.value })}></FormControl>
           <ul>
             {this.props.comments && this.props.comments.map(comment => <CommentListItem key={comment._id} comment={comment} /> )}
 
           </ul>
+          </Fragment>
         )
     }
 
